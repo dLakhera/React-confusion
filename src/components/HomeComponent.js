8d0c40fe8b72e6b743cc4c9132eb2fc80faccd32
droidLakhera <dlakhera02@gmail.com>
2021-02-06 14:24:05 +0530

    Breadcrumbs added

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle
+    CardTitle, CardSubtitle, Breadcrumb , BreadcrumbItem
 } from 'reactstrap';
+
+import { Link } from 'react-router-dom'
 
 function RenderCard({ item }) {
     console.log("RednerCard called");
@@ -23,6 +25,18 @@ function Home(props) {
     console.log(props);
     return (
         <div className="container">
+
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem active><Link to="/home">Home</Link></BreadcrumbItem>
+                    {/* <BreadcrumbItem active>Menu</BreadcrumbItem> */}
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Home</h3>
+                    <hr />
+                </div>
+            </div>
+
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.dish} />
